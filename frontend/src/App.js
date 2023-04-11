cd22eb7f62a046ea4f601e38f3462d0b3a1dc1b1
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2023-04-11 15:23:21 +0530

    more changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
+import abi from "./utils/WavePortal.json";
 
 const getEthereumObject = () => window.ethereum;
 
@@ -9,8 +10,8 @@ const findMetaMaskAccount = async () => {
     const ethereum = getEthereumObject();
 
     /*
-    * First make sure we have access to the Ethereum object.
-    */
+     * First make sure we have access to the Ethereum object.
+     */
     if (!ethereum) {
       console.error("Make sure you have Metamask!");
       return null;
@@ -34,8 +35,10 @@ const findMetaMaskAccount = async () => {
 };
 
 export default function App() {
-
   const [currentAccount, setCurrentAccount] = useState("");
+  const [numberOfWaves, setNumberOfWaves] = useState(0);
+  const contractAddress = "0x0CF4464C948B21e665571ad46C2170c43c334aC2";
+  const contractABI = abi.abi;
 
   const connectWallet = async () => {
     try {
@@ -56,6 +59,43 @@ export default function App() {
     }
   };
 
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+         * Execute the actual wave from your smart contract
+         */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        setNumberOfWaves(count.toNumber());
+        
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     findMetaMaskAccount().then((account) => {
       if (account !== null) {
@@ -67,17 +107,14 @@ export default function App() {
   
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+        <div className="header">👋 Hey there!</div>
 
         <div className="bio">
-        Hey there, I am Dwaipayan and I am again starting my WEb3 Journey.
+          Hey there, I am Dwaipayan and I am again starting my WEb3 Journey.
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
@@ -90,6 +127,9 @@ export default function App() {
           </button>
         )}
 
+        {currentAccount ? `Connected Address: ${currentAccount}` : ``}
+
+        <div>Waves:{numberOfWaves}</div>
       </div>
     </div>
   );
