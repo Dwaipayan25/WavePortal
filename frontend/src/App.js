084874f440ec2dc3482f97fd2150c268234a4792
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2023-04-11 17:01:11 +0530

    frontend modified accordint to modified web portal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,136 +1,139 @@
-import React, { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import "./App.css";
-import abi from "./utils/WavePortal.json";
+import WavePortal2 from "./utils/WavePortal2.json";
 
-const getEthereumObject = () => window.ethereum;
+function App() {
+  const [message, setMessage] = useState("");
+  const [value, setValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [waves, setWaves] = useState([]);
+  const contractAddress = "0xFCaC01B43052Ace67D37fC3cc7B13cAd87c285Dd";
+  const contractABI = WavePortal2.abi;
 
-const findMetaMaskAccount = async () => {
-  try {
-    const ethereum = getEthereumObject();
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    /*
-     * First make sure we have access to the Ethereum object.
-     */
-    if (!ethereum) {
-      console.error("Make sure you have Metamask!");
-      return null;
-    }
-
-    console.log("We have the Ethereum object", ethereum);
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account:", account);
-      return account;
+  const connectMetamask = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const account = accounts[0];
+        localStorage.setItem("account", account); // Store the account in local storage
+        setAccount(account);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to connect to Metamask. Please try again.");
+      }
     } else {
-      console.error("No authorized account found");
-      return null;
-    }
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-export default function App() {
-  const [currentAccount, setCurrentAccount] = useState("");
-  const [numberOfWaves, setNumberOfWaves] = useState(0);
-  const contractAddress = "0x0CF4464C948B21e665571ad46C2170c43c334aC2";
-  const contractABI = abi.abi;
-
-  const connectWallet = async () => {
-    try {
-      const ethereum = getEthereumObject();
-      if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
-      }
-
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const wave = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        );
-
-        let count = await wavePortalContract.getTotalWaves();
-        console.log("Retrieved total wave count...", count.toNumber());
-
-        /*
-         * Execute the actual wave from your smart contract
-         */
-        const waveTxn = await wavePortalContract.wave();
-        console.log("Mining...", waveTxn.hash);
-
-        await waveTxn.wait();
-        console.log("Mined -- ", waveTxn.hash);
-
-        count = await wavePortalContract.getTotalWaves();
-        setNumberOfWaves(count.toNumber());
-        
-        console.log("Retrieved total wave count...", count.toNumber());
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    } catch (error) {
-      console.log(error);
+      setError("Please install Metamask to use this app.");
     }
   };
 
   useEffect(() => {
-    findMetaMaskAccount().then((account) => {
-      if (account !== null) {
-        setCurrentAccount(account);
-      }
-    });
+    const handleAccountsChanged = (accounts) => {
+      setAccount(accounts[0]);
+    };
+    const storedAccount = localStorage.getItem("account"); // Retrieve the account from local storage
+    if (storedAccount) {
+      setAccount(storedAccount);
+    }
+
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+
+      const getWaves = async () => {
+        const waves = await contract.getAllWaves();
+        setWaves(waves);
+      };
+
+      getWaves();
+    }
   }, []);
 
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+
+      const waveTx = await contract.wave(message, {
+        value: ethers.utils.parseEther(value),
+      });
+      await waveTx.wait();
+
+      setMessage("");
+      setValue("");
+      setIsLoading(false);
+      setError(null);
+    } catch (err) {
+      console.error(err);
+      setIsLoading(false);
+      setError("Something went wrong. Please try again.");
+    }
+  };
+
   return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">👋 Hey there!</div>
-
-        <div className="bio">
-          Hey there, I am Dwaipayan and I am again starting my WEb3 Journey.
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
-
-        {/*
-         * If there is no currentAccount render this button
-         */}
-        {!currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
-        )}
-
-        {currentAccount ? `Connected Address: ${currentAccount}` : ``}
-
-        <div>Waves:{numberOfWaves}</div>
+    <div>
+      <h1>Wave Portal</h1>
+      {!account ? (
+        <button onClick={connectMetamask}>Connect to Metamask</button>
+      ) : (
+        <>
+          <p>Connected account: {account}</p>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Message:
+              <input
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </label>
+            <label>
+              Value (in ETH):
+              <input
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </label>
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Loading..." : "Wave"}
+            </button>
+          </form>
+          {error && <p>{error}</p>}
+        </>
+      )}
+      <div className="mt-4">
+        <h2>Waves</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>From</th>
+              <th>Message</th>
+              <th>Timestamp</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {waves.map((wave, index) => (
+              <tr key={index}>
+                <td>{wave.waver}</td>
+                <td>{wave.message}</td>
+                <td>{new Date(wave.timestamp * 1000).toLocaleString()}</td>
+                <td>{wave.value.toString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 }
+
+export default App;
